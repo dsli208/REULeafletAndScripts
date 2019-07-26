e37e5852329e014245e673f77ef9b52118af7701
David S. Li <ds.li@outlook.com>
2019-07-26 15:45:10 -0400

    Update intersection_encounters

--- intersection_encounters.php
+++ intersection_encounters.php
@@ -20,7 +20,7 @@
 <head>
 
 <title>Test Map 2</title>
-
+<!-- Add leaflet sources -->
 <link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
 	integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
 crossorigin=""/>
